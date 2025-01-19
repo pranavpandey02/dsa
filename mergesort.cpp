447fcbbe432a2db772b7b8669fa93090ad3aeafa
pranavpandey02 <pranavpandey479@gmail.com>
2025-01-19 23:37:55 +0530

    commting the value

---
+++ mergesort.cpp
@@ -0,0 +1,50 @@
+ #include<iostream>
+ using namespace std;
+ void merges(int A[], int low, int mid, int high)
+ {
+    int i,j,k,B[high+1];
+    i=low;
+    j=mid+1;
+    k=low;
+    while (i<=mid && j<=high)
+    {
+        if (A[i]=A[j])
+        {
+            B[k]=A[i];
+            i++;
+        }
+        else
+        {
+           B[k]=A[j];
+           j++;
+        }
+        k++;
+    }
+    while(i<=mid)
+    {
+        B[k]=A[i];
+        k++;
+        i++;
+    }
+    while(j<=high)
+    {
+        B[k]=A[j];
+        k++;
+        j++;
+    }
+    for(i=0;i<=high;i++)
+    {
+        A[i]=B[i];
+    }
+ }
+ void divide (int A[],int low,int high)
+ {
+    int mid;
+    if(low<high)
+    {
+        mid = (low+high)/2;
+        divide(A,low,mid);
+        divide (A,mid+1,high);
+        merges(A[],low,mid,high);
+    }
+ }
